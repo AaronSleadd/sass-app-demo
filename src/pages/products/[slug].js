feb01c52576e248a8b68fbd2c5953335523a9169
Aaron <aaronsleadd@Aarons-MacBook-Pro-2.local>
2023-11-07 15:43:48 -0800

    Updating UI for several components

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,1 +1,27 @@
-export default function ProductPage() {}
+import { useRouter } from "next/router";
+
+
+export default function ProductPage() {
+    const router = useRouter();
+    console.log(router.query.slug);
+
+    return (
+        <>
+            <div className="section bg-blue">
+                <div className="container">
+                    <div className="section-intro">
+                        <h1>The Latest Products</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="section small">
+                <div className="container">
+                    <ul className="product-card-grid"></ul>
+                </div>
+            </div>
+            
+            <h1>{router.query.slug}</h1>
+        </>
+    )
+}
